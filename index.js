99db4975ccb4aebceed4e1d9ab074a6075031061
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-11-19 07:50:00 +0530

    commit on terminal

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/user", userrouter);
 app.use("/note", noteRouter);
 app.get("/", (req, res) => {
   res.send({
@@ -25,3 +24,5 @@ app.listen(port, async () => {
   }
   console.log("The server is running on port number", port);
 });
+
+app.use("/user", userrouter);
